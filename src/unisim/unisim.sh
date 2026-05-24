45bf73454bc30d40d8127b3cc76e65e35bcac24d
David R. <davidrubio97@outlook.com>
2026-05-24 13:24:31 +0200

    update

--- src/unisim/unisim.sh
+++ src/unisim/unisim.sh
@@ -1,4 +1,12 @@
+# GHDL
 
-ghdl -a --std=08 -fsynopsys --work=unisim unisim/vcomponents/*.vhd
-ghdl -a --std=08 -fsynopsys --work=unisim unisim/primitives/*.vhd
-ghdl -a --std=08 -fsynopsys --work=unisim unisim/retarget/*.vhd
+# ghdl -a --std=08 -fsynopsys --work=unisim unisim/vcomponents/*.vhd
+# ghdl -a --std=08 -fsynopsys --work=unisim unisim/primitives/*.vhd
+# ghdl -a --std=08 -fsynopsys --work=unisim unisim/retarget/*.vhd
+
+
+# NVC
+
+# nvc --work=unisim -M 32m -a unisim/vcomponents/*.vhd
+# nvc --work=unisim -M 32m -a unisim/primitives/*.vhd
+# nvc --work=unisim -M 32m -a unisim/retarget/*.vhd
